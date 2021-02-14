41e5664adbaff65e6d9d3545003a0f6075edd32d
Abban-Fahim <67863502+Abban-Fahim@users.noreply.github.com>
2021-02-14 08:09:22 +0400

    Update skills.js

--- skills.js
+++ skills.js
@@ -1,27 +1,27 @@
 const skills = [
 	{
 		title: 'Node JS',
-		desc: 'The <a href="https://insights.stackoverflow.com/survey/2020#technology-other-frameworks-libraries-and-tools-all-respondents3">most popular and in-demand</a> server-side lanuage today, Node JS has a simple and robust networking platform for applications of all scale.',
+		desc: '',
 		img: 'nodejs'
 	},
 	{
 		title: 'JQuery',
-		desc: 'JQuery is a fast and lightweight front-end framework for Javascript, and a <a href="https://insights.stackoverflow.com/survey/2020#technology-web-frameworks-all-respondents2">very in-demand technology</a> for front-end development.',
+		desc: '',
 		img: 'jquery'
 	},
 	{
 		title: 'Bootstrap',
-		desc: 'Bootstrap is a front-end framework made by the dev team at <a href="https://twitter.com">Twitter</a> to allow ease and speed in making beautiful and responsive websites.',
+		desc: '',
 		img: 'bootstrap'
 	},
 	{
 		title: 'React',
-		desc: 'React is a component based framework used by many companies such as <a href="https://facebook.com/">Facebook</a> and <a href="https://airbnb.com/">Airbnb</a>, to build user interfaces.',
+		desc: '',
 		img: 'react'
 	},
 	{
 		title: 'MongoDB',
-		desc: 'One of the fastest and most compact database management systems used by big companies like <a href="https://ebay.com">Ebay</a> and <a href="https://ea.com/">EA Sports</a>.',
+		desc: '',
 		img: 'mongo'
 	}
 ];
